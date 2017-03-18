456d1f68241d41332fa4fc80ae055e2dbd0cb976
vernistage <sdickers10@gmail.com>
2017-03-18 12:34:01 -0500

    add initial users model w/no relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # Validations
+  validates :first_name, :last_name, :title, :city, :state, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
