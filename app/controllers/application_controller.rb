38f045280bd8d169117fb7a0abd60f469abc9690
vernistage <sdickers10@gmail.com>
2017-03-22 12:38:40 -0500

    succesful registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # Devise shennanigans
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_registration_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:first_name, :last_name, :email, :title, :studio, :city, :state)
+  def configure_registration_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:first_name, :last_name, :email, :password, :title, :studio, :city, :state)
     end
   end
 end
