286052c4ebafe702538b2668fceb65a1934696b0
vernistage <sdickers10@gmail.com>
2017-04-02 19:53:16 -0500

    initiate events routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # Devise
   devise_for :users, controllers: { sessions: 'devise/sessions' }
-
   resources :users
+  resources :events
+  
   root to: "home#index"
 end
